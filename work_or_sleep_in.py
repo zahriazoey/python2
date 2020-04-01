64caefd6ff2e43a2fbc79022c4e10e4a52d5dc96
Zahria Shannon <zahria.shannon08@gmail.com>
2020-04-01 14:56:40 -0400

    work or sleep

---
+++ work_or_sleep_in.py
@@ -0,0 +1,15 @@
+day = int(input('Day (0-6)? '))
+if day == 0:
+    print("Sleep in")
+elif day == 1:
+    print('Go to work')
+elif day == 2: 
+    print('Go to work')
+elif day == 3:
+    print('Go to work')
+elif day == 4: 
+    print('Go to work')
+elif day == 5: 
+    print('Go to work')
+else:  
+    print('Sleep in')
