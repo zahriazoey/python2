bb5e253dcbf5cfdb71f4b00aaa95eb77acd91a54
Zahria Shannon <zahria.shannon08@gmail.com>
2020-04-01 20:28:26 -0400

    temperature

---
+++ degree_conversion.py
@@ -0,0 +1,7 @@
+temp_c = float(input('Temperature in C? '))
+
+temp_f = float(temp_c * 9/2) + 32
+
+sum = f'{temp_f} F'
+
+print(sum)
--- work_or_sleep_in.py
+++ work_or_sleep_in.py
@@ -1,15 +1,5 @@
 day = int(input('Day (0-6)? '))
-if day == 0:
-    print("Sleep in")
-elif day == 1:
-    print('Go to work')
-elif day == 2: 
-    print('Go to work')
-elif day == 3:
-    print('Go to work')
-elif day == 4: 
-    print('Go to work')
-elif day == 5: 
-    print('Go to work')
-else:  
+if day == 0 or day == 6:
     print('Sleep in')
+else:
+    print('Go to work')
